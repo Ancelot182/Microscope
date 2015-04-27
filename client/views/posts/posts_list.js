63696121dc56a0a0efb6b3d76de6dfc3dbfdd1b5
Ancelot182 <ancelot182@gmail.com>
2015-04-27 15:46:13 +0200

    Microscope - aggiunta collezione collegata nel template 'postsList'

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,25 +1,28 @@
 /**
  * Created by IVAN on 27/04/2015.
  */
-var postData = [
+//var postData = [
+//
+//    {
+//        title: 'Introducing telescope',
+//        author: 'Sasha Greif',
+//        url: 'http://sachagreif.com/introducing-telescope/'
+//    },
+//    {
+//        title: 'Meteor',
+//        author: 'Tom Coleman',
+//        url: 'http://meteor.com'
+//    },
+//    {
+//        title: 'The Meteor Book',
+//        author: 'Tom Coleman',
+//        url: 'http://themeteorbook.com'
+//    }
+//
+//];
 
-    {
-        title: 'Introducing telescope',
-        author: 'Sasha Greif',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        author: 'Tom Coleman',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
-    }
-
-];
 Template.postsList.helpers({
-    posts: postData
+    posts: function(){
+        return Posts.find();
+    }
 })
