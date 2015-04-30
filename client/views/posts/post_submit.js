2abf61c2c42462e142318aaa7748013701fdd6f1
Ancelot182 <ancelot182@gmail.com>
2015-04-30 16:03:23 +0200

    Microscope - Utilizzato un metodo per inserire un post

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -12,7 +12,14 @@ Template.postSubmit.events({
             message: $(e.target).find('[name=message]').val()
         }
 
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+        //post._id = Posts.insert(post);
+
+        Meteor.call('post',post, function (error, id) {
+            if(error){
+                return alert(error.reason);
+            }
+
+            Router.go('postPage', post);
+        });
     }
 })
--- collections/posts.js
+++ collections/posts.js
@@ -4,9 +4,48 @@
 
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-    insert: function(userId, doc){
-        // Allowing insertion just for logged user
-        return !! userId;
+//Posts.allow({
+//    insert: function(userId, doc){
+//        // Allowing insertion just for logged user
+//        return !! userId;
+//    }
+//})
+
+Meteor.methods({
+
+    post: function(postAttributes){
+
+        var user = Meteor.user(),
+            postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+        // Check user logged in
+        if(!user){
+            throw new Meteor.Error(401, "You need to be logged in to post new stories");
+        }
+
+        // Ensure the post has a title
+        if(!postAttributes.title){
+            throw new Meteor.Error(422, "Please fill in the headline");
+        }
+
+        // Check there're no post with the same url
+        if(postAttributes.url && postWithSameLink){
+            throw new Meteor.Error(302, "This link has already been posted", postWithSameLink._id);
+        }
+
+        // Pick out the whitelisted keys
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+
+            userId: user._id,
+            author: user.username,
+            submitted: new Date().getTime()
+
+        });
+
+        var postId = Posts.insert(post);
+
+        return postId;
     }
-})
+
+
+});
